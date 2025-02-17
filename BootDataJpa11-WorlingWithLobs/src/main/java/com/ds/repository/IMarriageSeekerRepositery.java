568dabfe8002c231b86f0649179119204051942a
Durga Sai Kodali <KODALISAI889@GMAIL.COM>
2025-02-17 16:22:19 +0530

    Delete BootDataJpa11-WorlingWithLobs directory

--- BootDataJpa11-WorlingWithLobs/src/main/java/com/ds/repository/IMarriageSeekerRepositery.java
+++
@@ -1,8 +0,0 @@
-package com.ds.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ds.entity.MarriageSeeker;
-
-public interface IMarriageSeekerRepositery extends JpaRepository<MarriageSeeker, Integer> {
-}
